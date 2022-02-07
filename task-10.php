22697c9617543320928c4aa973375376882087bc
azizovm97 <50250969+azizovm97@users.noreply.github.com>
2022-02-07 21:56:26 +0500

    Update task-10.php
    Edit task-10

--- task-10.php
+++ task-10.php
@@ -7,6 +7,7 @@ function sendHuman ($ship)
     "атакующий", 
     "представитель"
    ];
+   
    if($ship == "Пират"){
       array_push($message, $people[1]);
    }
@@ -14,9 +15,13 @@ function sendHuman ($ship)
       array_push($message, $people[2]);
    }
    
-   else{ array_push($message, $people[0]);}
-     
-   implode(", ", $message);
-   return $message;
+   if($ship == "Торговое судно") { 
+      array_push($message, $people[0]);
+   } 
+
+        return $message
+     ? implode(',\n', $message)
+     :'No validation messages';
 }
+
 echo sendHuman("Пират");
